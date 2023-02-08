be6d0d8310f88ef7f4064c938d62fef323797055
Milton Gomez <milton.gomez@unil.ch>
2023-02-08 08:31:57 +0100

    Added GPU Ready containers

---
+++ TCache/tf-gpu/env_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /opt/miniconda/etc/profile.d/conda.sh
+export PATH=/opt/miniconda/bin:$PATH
+export PYTHONPATH=/opt/miniconda3/bin:$PATH
+export CONDA_ALWAYS_YES="true"
+
+conda update -n base conda
+conda install -c conda-forge 'python<=3.9.15'
+conda install -c conda-forge mamba
+CONDA_OVERRIDE_CUDA="11.2" mamba env create -f /opt/dev_env.yml -q
+
+conda clean --all --yes
+
+export CONDA_ALWAYS_YES="false"
